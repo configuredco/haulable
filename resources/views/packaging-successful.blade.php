dce9f8cdcc98c16fd1cfe5de60efa59d746771a6
De Langhe Thibo <38117009+thibodelanghe@users.noreply.github.com>
2023-03-28 14:06:19 +0200

    Add the resource folder to box settings for build and fixed vars

--- resources/views/packaging-successful.blade.php
+++ resources/views/packaging-successful.blade.php
@@ -1,9 +1,9 @@
 <div class="space-y-1 mb-1">
     <div>
-        📦 <span class="bg-green-300 text-black px-1">Packaging for {$platform->value} successful.</span>
+        📦 <span class="bg-green-300 text-black px-1">Packaging for {{ $platform->value }} successful.</span>
     </div>
     <div>
-        ✅ <a href="{$dir}/{$buildName}{$fileExtension}">{$dir}/{$buildName}{$fileExtension}</a>
+        ✅ <a href="{{ $dir }}/{{ $buildName }}{{ $fileExtension }}">{{ $dir }}/{{ $buildName }}{{ $fileExtension }}</a>
     </div>
 </div>
 <br>
